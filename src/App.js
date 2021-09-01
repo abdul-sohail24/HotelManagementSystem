2a7c5840048accf50cbf6532ffdb93d9f8aafe2d
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-01 23:03:10 +0530

    Added more features to the code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from "react";
 
 // functional component
-function App() {
-	return <h1>Hello</h1>;
-}
+
+// const App() {
+// 	return (
+// 		<div>
+// 			<h3>THE DARK KNIGHT</h3>
+// 		</div>
+// 	);
+// }
+
+// Using const
+const App = () => {
+	// fat arrow function (=>)
+	return (
+		<>
+			<MyName />
+			<h2 className="hello">Christian Bale {2 + 5} </h2>
+			{/* Js is written in curly braces */}
+			<MyName />
+		</>
+	);
+};
+
+// const keyword
+const MyName = () => {
+	return <h3>I'm Iron Man</h3>;
+};
 
 export default App;
