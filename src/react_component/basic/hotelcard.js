65f8cb83e2a93b0f9af281c233a01cd0ff4e12c0
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-03 17:18:06 +0530

    Cleaned the code

--- src/react_component/basic/hotelcard.js
+++ src/react_component/basic/hotelcard.js
@@ -1,31 +1,31 @@
 import React from "react";
 
 const Hotelcard = ({ menuData }) => {
-	console.log(menuData);
+	// console.log(menuData);
 	const myStyle = { color: "blue" };
 
 	return (
 		<>
 			<section className="main-card--container">
 				{menuData.map((curElem) => {
+					const { id, name, category, image, description } = curElem;
+
 					return (
 						<>
 							<div className="card-container">
 								<div className="card">
 									<div className="card-body">
-										<span className="card-number card-circle subtle">
-											{curElem.id}
-										</span>
+										<span className="card-number card-circle subtle">{id}</span>
 										<span className="card-author subtle" style={myStyle}>
-											{curElem.category}
+											{category}
 										</span>
-										<h3 className="card-title"> {curElem.name} </h3>
+										<h3 className="card-title"> {name} </h3>
 										<span className="card-description subtle">
-											{curElem.description}
+											{description}
 										</span>
 										<div className="card-read"> Read </div>
 									</div>
-									<img src={curElem.image} alt="" className="card-media" />
+									<img src={image} alt="" className="card-media" />
 
 									<span className="card-tag subtle">Order Now</span>
 								</div>
