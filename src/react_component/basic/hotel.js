4e5d16b2eb7aebd62970ef048c8d949761e4d82e
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-03 07:21:16 +0530

    Created Cards

--- src/react_component/basic/hotel.js
+++ src/react_component/basic/hotel.js
@@ -1,33 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import Items from "./hotelapi";
+import Hotelcard from "./hotelcard";
 
 const Hotel = () => {
-	const myStyle = { color: "blue" };
+	// Hooks should always be at Top
+	const [menuData, setMenuData] = useState(Items);
+
+	console.log(menuData);
 
 	return (
 		<>
-			<div className="card-container">
-				<div className="card">
-					<div className="card-body">
-						<span className="card-number card-circle subtle">1</span>
-						<span className="card-author subtle" style={myStyle}>
-							Breakfast
-						</span>
-						<h3 className="card-title"> Maggie </h3>
-						<span className="card-description subtle">
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias,
-							optio repellat veritatis officiis earum quae, tempora dignissimos
-							necessitatibus ipsam id incidunt amet, corrupti provident quidem
-							vel molestias eaque. Ut, cum.
-						</span>
-						<div className="card-read"> Read </div>
-					</div>
-					<img src="{image}" alt="" className="card-media" />
-
-					<span className="card-tag subtle">Order Now</span>
-				</div>
-			</div>
-			{/* <h2>Welcome to my Restaurent</h2> */}
+			<Hotelcard />
 		</>
 	);
 };
