462c8ec9152fe60defb8e44e73314a66237824d3
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-03 18:11:41 +0530

    Cleaned the code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from "react";
 import Hotel from "./react_component/basic/hotel";
 
-// functional component
-
-// const App() {
-// 	return (
-// 		<div>
-// 			<h3>THE DARK KNIGHT</h3>
-// 		</div>
-// 	);
-// }
-
 // Using const
 const App = () => {
 	// fat arrow function (=>)
 	return (
 		<>
 			<Hotel />
-			{/* <MyName /> */}
-			{/* <h2 className="hello">Christian Bale {2 + 5} </h2> */}
-			{/* Js is written in curly braces */}
-			{/* <MyName /> */}
 		</>
 	);
 };
-
-// const keyword
-// const MyName = () => {
-// 	return <h3>I'm Iron Man</h3>;
-// };
 
 export default App;
