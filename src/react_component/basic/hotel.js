a74114281ae1a5cd79d1b50c7eefdb887d7bdd74
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-03 08:20:54 +0530

    Completed adding NavBar

--- src/react_component/basic/hotel.js
+++ src/react_component/basic/hotel.js
@@ -7,10 +7,50 @@ const Hotel = () => {
 	// Hooks should always be at Top
 	const [menuData, setMenuData] = useState(Items);
 
+	const filterItem = (category) => {
+		const updatedList = Items.filter((curElem) => {
+			return curElem.category === category;
+		});
+		setMenuData(updatedList);
+	};
 	// console.log(menuData);
 
 	return (
 		<>
+			<nav className="navbar">
+				<div className="btn-group">
+					<button
+						className="btn-group__item"
+						onClick={() => filterItem("breakfast")}
+					>
+						Breakfast
+					</button>
+					<button
+						className="btn-group__item"
+						onClick={() => filterItem("lunch")}
+					>
+						Lunch
+					</button>
+					<button
+						className="btn-group__item"
+						onClick={() => filterItem("evening")}
+					>
+						Evening
+					</button>
+					<button
+						className="btn-group__item"
+						onClick={() => filterItem("dinner")}
+					>
+						Dinner
+					</button>
+					<button
+						className="btn-group__item"
+						onClick={() => setMenuData(Items)}
+					>
+						All
+					</button>
+				</div>
+			</nav>
 			<Hotelcard menuData={menuData} />
 		</>
 	);
