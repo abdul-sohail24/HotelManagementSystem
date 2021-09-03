56059ff49a96db1c8d46854220f08b8e0f5e5f2e
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-03 07:43:51 +0530

    Completed the hotel code

--- src/react_component/basic/hotel.js
+++ src/react_component/basic/hotel.js
@@ -7,11 +7,11 @@ const Hotel = () => {
 	// Hooks should always be at Top
 	const [menuData, setMenuData] = useState(Items);
 
-	console.log(menuData);
+	// console.log(menuData);
 
 	return (
 		<>
-			<Hotelcard />
+			<Hotelcard menuData={menuData} />
 		</>
 	);
 };
