285e77b0c84272033ed8962ff940212311b8bad0
Abdul Sohail <abdulsohail22246@gmail.com>
2021-09-03 07:41:49 +0530

    Completed loop for Cards

--- src/react_component/basic/hotelcard.js
+++ src/react_component/basic/hotelcard.js
@@ -1,31 +1,43 @@
 import React from "react";
 
-const Hotelcard = () => {
+const Hotelcard = ({ menuData }) => {
+	console.log(menuData);
 	const myStyle = { color: "blue" };
 
 	return (
-		<div className="card-container">
-			<div className="card">
-				<div className="card-body">
-					<span className="card-number card-circle subtle">1</span>
-					<span className="card-author subtle" style={myStyle}>
-						Breakfast
-					</span>
-					<h3 className="card-title"> Maggie </h3>
-					<span className="card-description subtle">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias,
-						optio repellat veritatis officiis earum quae, tempora dignissimos
-						necessitatibus ipsam id incidunt amet, corrupti provident quidem vel
-						molestias eaque. Ut, cum.
-					</span>
-					<div className="card-read"> Read </div>
-				</div>
-				<img src="{image}" alt="" className="card-media" />
+		<>
+			<section className="main-card--cointainer">
+				{menuData.map((curElem) => {
+					return (
+						<>
+							<div className="card-container">
+								<div className="card">
+									<div className="card-body">
+										<span className="card-number card-circle subtle">1</span>
+										<span className="card-author subtle" style={myStyle}>
+											Breakfast
+										</span>
+										<h3 className="card-title"> Maggie </h3>
+										<span className="card-description subtle">
+											Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+											Alias, optio repellat veritatis officiis earum quae,
+											tempora dignissimos necessitatibus ipsam id incidunt amet,
+											corrupti provident quidem vel molestias eaque. Ut, cum.
+										</span>
+										<div className="card-read"> Read </div>
+									</div>
+									<img src="{image}" alt="" className="card-media" />
 
-				<span className="card-tag subtle">Order Now</span>
-			</div>
-		</div>
-		/* <h2>Welcome to my Restaurent</h2> */
+									<span className="card-tag subtle">Order Now</span>
+								</div>
+							</div>
+						</>
+					);
+				})}
+			</section>
+
+			{/* <h2>Welcome to my Restaurent</h2> */}
+		</>
 	);
 };
 
